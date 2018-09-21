624912e7b080f17cab3029bc6ad405150427f65d
Alejandro villanueva M <alejandrovillmon@gmail.com>
2018-09-21 12:55:49 -0500

    Primer commit

---
+++ TableView1/ThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdViewController.swift
+//  TableView1
+//
+//  Created by Macbook on 21/09/18.
+//  Copyright © 2018 iosLab. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+   
+    
+
+   
+}
